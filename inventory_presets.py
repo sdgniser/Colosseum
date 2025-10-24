17c68d4f3dac225a94eb36ffec2f1f1d5fc2cdda
Sodashi <vaibhav.singh@niser.ac.in>
2025-10-24 05:36:04 +0530

    Revised healing values

--- inventory_presets.py
+++ inventory_presets.py
@@ -2,28 +2,28 @@ BASE_INVENTORY = {
     "Potion": {"count": 3, "effect": "heal", "value": 30, "description": "Heals 30 HP"},
     "Ether": {"count": 1, "effect": "heal", "value": 15, "description": "Heals 15 HP"},
     "Bomb": {"count": 2, "effect": "damage", "value": 40, "description": "Deals 40 damage"},
-    "Elixir": {"count": 0, "effect": "full_heal", "value": 0, "description": "Fully restores HP"}
+    "Elixir": {"count": 0, "effect": "heal", "value": 80, "description": "Heals 80 HP"}
 }
 
 # Class-specific starting inventories
 CLASS_STARTING_INVENTORY = {
     "Warrior": {
-        "Potion": {"count": 4, "effect": "heal", "value": 30, "description": "Heals 30 HP"},
-        "Ether": {"count": 1, "effect": "heal", "value": 15, "description": "Heals 15 HP"},
+        "Potion": {"count": 4, "effect": "heal", "value": 30, "description": "Heals 35 HP"},
+        "Ether": {"count": 1, "effect": "heal", "value": 15, "description": "Heals 20 HP"},
         "Bomb": {"count": 1, "effect": "damage", "value": 40, "description": "Deals 40 damage"},
-        "Elixir": {"count": 0, "effect": "full_heal", "value": 0, "description": "Fully restores HP"}
+        "Elixir": {"count": 0, "effect": "heal", "value": 80, "description": "Heals 80 HP"}
     },
     "Mage": {
-        "Potion": {"count": 2, "effect": "heal", "value": 30, "description": "Heals 30 HP"},
-        "Ether": {"count": 3, "effect": "heal", "value": 15, "description": "Heals 15 HP"},
+        "Potion": {"count": 2, "effect": "heal", "value": 30, "description": "Heals 35 HP"},
+        "Ether": {"count": 3, "effect": "heal", "value": 15, "description": "Heals 20 HP"},
         "Bomb": {"count": 2, "effect": "damage", "value": 40, "description": "Deals 40 damage"},
-        "Elixir": {"count": 1, "effect": "full_heal", "value": 0, "description": "Fully restores HP"}
+        "Elixir": {"count": 0, "effect": "heal", "value": 80, "description": "Heals 80 HP"}
     },
     "Rogue": {
-        "Potion": {"count": 3, "effect": "heal", "value": 30, "description": "Heals 30 HP"},
-        "Ether": {"count": 2, "effect": "heal", "value": 15, "description": "Heals 15 HP"},
+        "Potion": {"count": 3, "effect": "heal", "value": 30, "description": "Heals 35 HP"},
+        "Ether": {"count": 2, "effect": "heal", "value": 15, "description": "Heals 20 HP"},
         "Bomb": {"count": 3, "effect": "damage", "value": 40, "description": "Deals 40 damage"},
-        "Elixir": {"count": 0, "effect": "full_heal", "value": 0, "description": "Fully restores HP"}
+        "Elixir": {"count": 0, "effect": "heal", "value": 80, "description": "Heals 80 HP"}
     }
 }
 
@@ -32,4 +32,5 @@ EXTRA_ITEMS = {
     "Mega Potion": {"count": 1, "effect": "heal", "value": 50, "description": "Heals 50 HP"},
     "Smoke Bomb": {"count": 2, "effect": "escape", "value": 0, "description": "Allows escape from battle"},
     "Antidote": {"count": 2, "effect": "cure", "value": 0, "description": "Cures poison status"}
+
 }
