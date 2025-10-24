fef836258f4e6d4d8578899fe568bb5cb5414e66
Sodashi <vaibhav.singh@niser.ac.in>
2025-10-24 05:38:52 +0530

    Given 1 elixir to every class for extension

--- inventory_presets.py
+++ inventory_presets.py
@@ -11,19 +11,19 @@ CLASS_STARTING_INVENTORY = {
         "Potion": {"count": 4, "effect": "heal", "value": 30, "description": "Heals 35 HP"},
         "Ether": {"count": 1, "effect": "heal", "value": 15, "description": "Heals 20 HP"},
         "Bomb": {"count": 1, "effect": "damage", "value": 40, "description": "Deals 40 damage"},
-        "Elixir": {"count": 0, "effect": "heal", "value": 80, "description": "Heals 80 HP"}
+        "Elixir": {"count": 1, "effect": "heal", "value": 80, "description": "Heals 80 HP"}
     },
     "Mage": {
         "Potion": {"count": 2, "effect": "heal", "value": 30, "description": "Heals 35 HP"},
         "Ether": {"count": 3, "effect": "heal", "value": 15, "description": "Heals 20 HP"},
         "Bomb": {"count": 2, "effect": "damage", "value": 40, "description": "Deals 40 damage"},
-        "Elixir": {"count": 0, "effect": "heal", "value": 80, "description": "Heals 80 HP"}
+        "Elixir": {"count": 1, "effect": "heal", "value": 80, "description": "Heals 80 HP"}
     },
     "Rogue": {
         "Potion": {"count": 3, "effect": "heal", "value": 30, "description": "Heals 35 HP"},
         "Ether": {"count": 2, "effect": "heal", "value": 15, "description": "Heals 20 HP"},
         "Bomb": {"count": 3, "effect": "damage", "value": 40, "description": "Deals 40 damage"},
-        "Elixir": {"count": 0, "effect": "heal", "value": 80, "description": "Heals 80 HP"}
+        "Elixir": {"count": 1, "effect": "heal", "value": 80, "description": "Heals 80 HP"}
     }
 }
 
@@ -34,3 +34,4 @@ EXTRA_ITEMS = {
     "Antidote": {"count": 2, "effect": "cure", "value": 0, "description": "Cures poison status"}
 
 }
+
